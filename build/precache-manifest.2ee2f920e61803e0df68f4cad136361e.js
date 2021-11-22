1ab4907401f27df55f5682bea6c125febbc32bcd
Tamal <tamaldas177@gmail.com>
2021-11-22 14:41:14 +0530

    added new build

---
+++ build/precache-manifest.2ee2f920e61803e0df68f4cad136361e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a1a46e3b69328e0bebab244a01d29ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "349374938a4b55138643",
+    "url": "/static/css/main.47a3e70f.chunk.css"
+  },
+  {
+    "revision": "a84fc0bda2778009f76b",
+    "url": "/static/js/2.30c4d6fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.30c4d6fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "349374938a4b55138643",
+    "url": "/static/js/main.8bbbf51e.chunk.js"
+  },
+  {
+    "revision": "bb5b94e5bf8a2ac4b1d8",
+    "url": "/static/js/runtime-main.6fd1fea2.js"
+  },
+  {
+    "revision": "e7a85f8ca6e8bf0bacfc7ff08a617bcb",
+    "url": "/static/media/form_svg.e7a85f8c.svg"
+  }
+]);
